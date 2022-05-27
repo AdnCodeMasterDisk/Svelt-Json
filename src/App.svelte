798b8a47babf8d375909a57ff09c66956810fe96
adncode <deepartesdigitales@gmail.com>
2022-05-27 17:20:55 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+
+  <script>
+    // @ts-nocheck
+    
+      import '../src/assets/style.css'
+      import dataJson from './assets/data.json'
+    
+      //on line read
+      let arrayJson = []
+      let linkJson = 'https://raw.githubusercontent.com/AdnCodeMasterDisk/jsontest/main/array.json'
+    
+      const readJson = async() => {
+        const data = await
+        fetch(linkJson)
+        const json = await data.json()
+        arrayJson = json
+      }
+    
+      //inicializamos la funcion
+      readJson()
+     
+    </script>
+    
+    <h3>From local Json </h3>
+    <!--Local Json Read-->
+    {#each dataJson as element, i}
+            <p>
+                {i} - {element.username} - {element.email}
+            </p>
+    {/each}
+    
+    <h3>From Internet Json </h3>
+    <!-- on line read-->
+    {#each arrayJson as array, i}
+            <p>
+                {i} - {array.username} - {array.email}
+            </p>
+    {/each}
+    
+    
